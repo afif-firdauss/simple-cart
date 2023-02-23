32ed805aa0458158b1bf7c6c6ee41a2ee43c0431
afif-firdauss <81554163+afif-firdauss@users.noreply.github.com>
2023-02-23 09:15:52 +0700

    update carousel

---
+++ index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Carousel } from 'react-responsive-carousel';
+import noImage from 'assets/no-image.png';
+
+export default function Index({ product }) {
+  return (
+    <div>
+      {product && (
+        <Carousel
+          showIndicators={false}
+          statusFormatter={(current, total) => `${current}/${total}`}
+          showArrows
+          showThumbs
+        >
+          {product.images?.map((image, index) => {
+            return (
+              <div key={index}>
+                <img 
+                  src={image} 
+                  alt={`${product.name} ${index}`}
+                  title={product?.name}
+                  loading="eager"
+                  onError={({ currentTarget }) => {
+                    currentTarget.onerror = null;
+                    currentTarget.src = noImage;
+                  }}
+                />
+              </div>
+            );
+          })}
+        </Carousel>
+      )}
+    </div>
+  )
+}
